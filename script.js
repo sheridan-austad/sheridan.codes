a866d8aadf7e5a825cc6f973f5795e343072bb02
sheridan <sheridanmaustad@gmail.com>
2021-10-20 21:55:11 -0600

    Add contact info

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h3 = document.createElement('h3');
-h3.textContent = "Welcome to my Website!"; 
+h3.textContent = "Welcome to my Website!"; <br/>
 document.querySelector('body').appendChild(h3); 
