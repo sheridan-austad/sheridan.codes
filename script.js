65dba7dde6b4ec07b03ca3415dcece8f8191b725
sheridan <sheridanmaustad@gmail.com>
2021-10-20 21:50:58 -0600

    Work and progress

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement('h2');
-h2.textContent = "Welcome to my Website!"; 
-document.querySelector('body').appendChild(h2); 
+const h3 = document.createElement('h3');
+h3.textContent = "Welcome to my Website!"; 
+document.querySelector('body').appendChild(h3); 
